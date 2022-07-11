16690d7352baeddadeaeebb7bbabd6ce7adc3c9e
Jevin Sweval <jevinsweval@gmail.com>
2022-07-11 15:54:17 -0700

    HiDPI patch for font sizes

---
+++ questa_hidpi.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+from path import Path, TempDir
+from pytrofs.trofs import create, extract
+
+
+def real_main(args):
+    patch_file = (Path(__file__).parent / "hidpi.patch").normpath()
+    # tmp_dir = TempDir()
+    tmp_dir = Path("tmp")
+    extract(args.in_file, tmp_dir)
+    subprocess.check_call(["patch", "-p1"], stdin=open(patch_file), cwd=tmp_dir)
+    create(args.out_file, tmp_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="questa_hidpi.py")
+    parser.add_argument("-i", "--in", dest="in_file", required=True, help="trofs input archive.")
+    parser.add_argument("-o", "--out", dest="out_file", required=True, help="trofs output archive.")
+    args = parser.parse_args()
+    real_main(args)
+
+
+if __name__ == "__main__":
+    main()
